005d4bb2211e8106988fce05df1a1a6f737271f3
r4venj <r4venj@protonmail.com>
2024-06-14 00:46:22 -0300

    Lista Ligada

--- src/dataStructure/Tree.java
+++ src/dataStructure/Tree.java
@@ -106,9 +106,9 @@ public class Tree {
 
     public static void main(String[] args) {
         Tree tree = new Tree();
-        tree.insert(37);
-        tree.insert(11);
-        tree.insert(66);
+//        tree.insert(1);
+//        tree.insert(2);
+//        tree.insert(3);
 
 //        System.out.println(tree.root.value);
 //        System.out.println(tree.root.left.value);
@@ -117,6 +117,9 @@ public class Tree {
 //        System.out.println(tree.root.isLeaf());
 //        System.out.println(tree.root.left.isLeaf());
 
+        tree.insert(37);
+        tree.insert(11);
+        tree.insert(66);
         tree.insert(66);
         tree.insert(8);
         tree.insert(17);
